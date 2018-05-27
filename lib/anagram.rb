89a3b667a825b268c0d064f41849807cf11e7834
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-27 21:37:16 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+    
+  end
+
+  def match (words)
+
+    word_alpha = @word.chars.sort.join
+    matches=[]
+    words.each do |maybe_match|
+
+      if maybe_match.chars.sort.join == word_alpha
+        matches << maybe_match
+      end
+    end
+    matches
+  end
+
+end
